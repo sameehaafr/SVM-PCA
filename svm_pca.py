3c6f75684758d6ba483e8670b1009a789622ca5e
Sameeha Afrulbasha <sameehaafr@gmail.com>
2023-07-02 12:00:47 -0400

    import changes

--- svm_pca.py
+++ svm_pca.py
@@ -1,6 +1,7 @@
+import streamlit as st
 import pandas as pd 
-import numpy as np 
-%pip install matplotlib
+import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from sklearn.datasets import load_breast_cancer
