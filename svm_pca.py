328f10b1bef070bde66dfff234cc9c3881ed245a
Sameeha Afrulbasha <sameehaafr@gmail.com>
2023-07-02 13:26:42 -0400

    fixing random state

--- svm_pca.py
+++ svm_pca.py
@@ -41,13 +41,13 @@ kernel1 = 'rbf'
 
 # Build basic SVM model
 def build_basic_svm(X_train_scaled, y_train):
-    clf_svm = SVC()
-    clf_svm.fit(X_train_scaled,y_train, random_state=30)
+    clf_svm = SVC(random_state=30)
+    clf_svm.fit(X_train_scaled,y_train)
     return clf_svm
 
 def build_svm(C, gamma, kernel, X_train_scaled, y_train):
-    clf_svm = SVC(C=C, gamma=gamma, kernel=kernel)
-    clf_svm.fit(X_train_scaled,y_train, random_state=30)
+    clf_svm = SVC(C=C, gamma=gamma, kernel=kernel, random_state=30)
+    clf_svm.fit(X_train_scaled,y_train)
     return clf_svm
 
 # Evaluate the basic SVM model
@@ -86,7 +86,7 @@ def find_best_params(X_train_scaled, y_train):
     return c, gamma, kernel
 
 def scree_plot(X_train_scaled):
-    pca = PCA().fit(X_train_scaled, random_state=30)
+    pca = PCA(random_state=30).fit(X_train_scaled)
     per_var = np.round(pca.explained_variance_ratio_ * 100, decimals=1)
 
     fig, ax = plt.subplots()
@@ -99,7 +99,7 @@ def scree_plot(X_train_scaled):
 
 # Build the model with the optimal parameters and the reduced number of features
 def pca(X_train_scaled, X_test_scaled, y_train):
-    pca = PCA().fit(X_train_scaled, random_state=30)
+    pca = PCA(random_state=30).fit(X_train_scaled)
 
     X_train_pca = pca.fit_transform(X_train_scaled)
     X_test_pca = pca.transform(X_test_scaled)
